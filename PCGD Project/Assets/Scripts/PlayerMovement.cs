6c3232eb8fb06182778b5bf3a243cd0d41f3cf88
Lehvotin <ville.lehvo@gmail.com>
2022-01-26 10:55:51 +0200

    Primitive shooting mechanism with mouse

--- PCGD Project/Assets/Scripts/PlayerMovement.cs
+++ PCGD Project/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+
     public Rigidbody2D rb;
+    public Camera mainCamera;
+
     private Vector2 moveDirection;
+    private Vector2 mousePosition;
 
     // Update is called once per frame
     void Update()
@@ -19,16 +23,22 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+
     void PlayerInput()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
