c13e31fcffb41752e6edcc3f99d16e4993f8c48b
Lehvotin <ville.lehvo@gmail.com>
2022-03-02 21:22:43 +0200

    Canvas fixed and pauses and menus added to it

--- PCGD Project/Assets/Scripts/Shooting.cs
+++ PCGD Project/Assets/Scripts/Shooting.cs
@@ -9,18 +9,16 @@ public class Shooting : MonoBehaviour
     [SerializeField]
     GameObject bullet;
 
+
     void Update()
     {   
-        
-
+ 
         if(Input.GetButtonDown("Fire1"))
         {
             if (Time.timeScale > 0)
             {
                 Instantiate(bullet, shootPoint.position, shootPoint.rotation);
-            }
-            
-            
+            }        
         }
     }
 }
