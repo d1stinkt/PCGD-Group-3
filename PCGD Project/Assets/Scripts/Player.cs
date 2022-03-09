f4477f1d5b6173fa41a308de849e3101d9b4b2c8
Joonas <joonas.alitalo@gmail.com>
2022-03-09 13:23:40 +0200

    Added shield breaking noise

--- PCGD Project/Assets/Scripts/Player.cs
+++ PCGD Project/Assets/Scripts/Player.cs
@@ -154,6 +154,7 @@ public class Player : MonoBehaviour
     IEnumerator ArmorBreak()
     {
         armorIcon.SetActive(false);
+        AudioManager.Play("ShieldBreak");
         yield return new WaitForSeconds(1.0f);
         gm.armor = false;
     }
